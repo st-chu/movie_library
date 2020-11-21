4cc10e03caf3d5ced8553188c1e825f5c77cfc38
Marek Starzyk <stachu.ms9@gmail.com>
2020-11-21 18:10:36 +0100

    Add top_titles

--- movie_library.py
+++ movie_library.py
@@ -16,6 +16,11 @@ class Movie:
         return f'{self.title} ({self.publish_year})'
 
     def play(self, step=1):
+        '''
+        increases the number of views of the title by 1
+        :param step: int
+        :return: <NoneType>
+        '''
         self.views += step
 
 
@@ -34,6 +39,11 @@ class Series(Movie):
 
 
 def get_movies(movies_library_list):
+    '''
+    a function that filters the movies_library_list and returns a sorted list of movies
+    :param movies_library_list: list
+    :return: movies_list: list
+    '''
     movies_list = []
     for item in movies_library_list:
         if isinstance(item, Series) is not True:
@@ -42,6 +52,11 @@ def get_movies(movies_library_list):
 
 
 def get_series(movies_library_list):
+    '''
+    a function that filters the movies_library_list and returns a sorted list of series
+    :param movies_library_list: list
+    :return: series_list: list
+    '''
     series_list = []
     for item in movies_library_list:
         if isinstance(item, Series) is True:
@@ -50,15 +65,46 @@ def get_series(movies_library_list):
 
 
 def search(title, movies_library_list):
+    '''
+    function that searches for movies or series by title
+    :param title: str
+    :param movies_library_list: list
+    :return: object: <class__main__Movie> or <class__main__Series>
+    '''
     for item in movies_library_list:
         if item.title.lower() == title.lower():
             return item
 
 
 def generate_views(movie_library_list):
+    '''
+    a function that randomly selects an item from the library and then adds a random (ranging from 1 to 100)
+    number of plays
+    :param movie_library_list: list
+    :return: <NoneType>
+    '''
     index = random.randint(0, len(movie_library_list)-1)
     views = random.randint(0, 100)
     movie_library_list[index].play(views)
+
+
+def run_generate_views(movie_library_list):
+    '''
+    runs functions generate_vievs 10 times
+    :param movie_library_list: list
+    :return: <NoneType>
+    '''
+    step = 0
+    while step < 10:
+        generate_views(movie_library_list)
+        step += 1
+
+
+def top_titles(movies_library_list, how_many_titles):
+    _sorted = sorted(movies_library_list, key=lambda movie: movie.views, reverse=True)
+    print(f'  Top {how_many_titles} Titles:')
+    for index in range(how_many_titles):
+        print(f'  {index+1}. {_sorted[index]}, views {_sorted[index].views}')
 
 
 movie_library = [
@@ -67,5 +113,6 @@ movie_library = [
     Movie('Team America', 2002, 'comedy'),
     Movie('Amelia', 1997, 'comedy')
 ]
-generate_views(movie_library)
-print(get_movies(movie_library))
+
+run_generate_views(movie_library)
+top_titles(movie_library, 3)
